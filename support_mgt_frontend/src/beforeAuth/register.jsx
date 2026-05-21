7c4f8c1ba94f2fb65558fefd715b9ef80c6aefe3
akshay kumar <akshay@softradix.in>
2026-05-21 19:24:11 +0530

    change 1

--- support_mgt_frontend/src/beforeAuth/register.jsx
+++ support_mgt_frontend/src/beforeAuth/register.jsx
@@ -6,113 +6,124 @@ import bgImage from "../assets/image copy.png";
 function register() {
   return (
     <>
-          <Grid container>
-              
-        <Grid
-          size={{ xs: 12, md: 6 }}
-          sx={{
-            position: "relative",
-            overflow: "hidden",
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            "&::before": {
-              content: '""',
-              position: "absolute",
-              top: 0,
-              right: 0,
-              width: "100%",
-              height: "100%",
-              backgroundImage: `url(${bgImage})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              opacity: 0.12,
-
-              objectFit: "cover",
-            },
-          }}
-        >
+      <Grid container>
+        <Grid size={{ xs: 12, md: 6 }}>
           <Box
-            component="form"
             sx={{
-              width: "100%",
-              maxWidth: 400,
-              borderRadius: 3,
-              border: "2px solid #ad3891",
-              boxShadow: 10,
-              p: 4,
-              position: "relative",
-              zIndex: 2,
+              display: "flex",
+              justifyContent: "center",
+              color: "#ad3891",
+              position: "absolute",
+              top: "120px",
+              left: "150px",
             }}
           >
-            <TextField
-              fullWidth
-              label="Email"
-              margin="normal"
+            <h1>Log In to your Account</h1>
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+              overflow: "hidden",
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              "&::before": {
+                content: '""',
+                position: "absolute",
+                top: 0,
+                right: 0,
+                width: "100%",
+                height: "100%",
+                backgroundImage: `url(${bgImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                opacity: 0.12,
+                objectFit: "cover",
+              },
+            }}
+          >
+            <Box
+              component="form"
               sx={{
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: "#ad3891 !important",
-                  },
-
-                  // Focus Border
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#ad3891",
-                  },
-                },
-
-                // Label Focus
-                "& .MuiInputLabel-root.Mui-focused": {
-                  color: "#ad3891",
-                },
-              }}
-            />
-            <br />
-
-            <TextField
-              fullWidth
-              label="Password"
-              type="password"
-              margin="normal"
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: "#ad3891 !important",
-                  },
-                  // Focus Border
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#ad3891",
-                  },
-                },
-                // Label Focus
-                "& .MuiInputLabel-root.Mui-focused": {
-                  color: "#ad3891",
-                },
-              }}
-            />
-            <br />
-            <br />
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{
-                width: "200px",
-                alignItems: "center",
-                py: 1.2,
-                borderRadius: 2,
-                backgroundColor: "#ad3891",
-
-                "&:hover": {
-                  backgroundColor: "#922f79",
-                },
+                width: "100%",
+                maxWidth: 400,
+                borderRadius: 3,
+                border: "2px solid #ad3891",
+                boxShadow: 10,
+                p: 4,
+                position: "relative",
+                zIndex: 2,
               }}
             >
-              Login
-            </Button>
+              <TextField
+                fullWidth
+                label="Email"
+                margin="normal"
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "&:hover fieldset": {
+                      borderColor: "#ad3891 !important",
+                    },
+
+                    // Focus Border
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#ad3891",
+                    },
+                  },
+
+                  // Label Focus
+                  "& .MuiInputLabel-root.Mui-focused": {
+                    color: "#ad3891",
+                  },
+                }}
+              />
+              <br />
+
+              <TextField
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "&:hover fieldset": {
+                      borderColor: "#ad3891 !important",
+                    },
+                    // Focus Border
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#ad3891",
+                    },
+                  },
+                  // Label Focus
+                  "& .MuiInputLabel-root.Mui-focused": {
+                    color: "#ad3891",
+                  },
+                }}
+              />
+              <br />
+              <br />
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  width: "200px",
+                  alignItems: "center",
+                  py: 1.2,
+                  borderRadius: 2,
+                  backgroundColor: "#ad3891",
+
+                  "&:hover": {
+                    backgroundColor: "#922f79",
+                  },
+                }}
+              >
+                Login
+              </Button>
+            </Box>
           </Box>
         </Grid>
         <Grid
